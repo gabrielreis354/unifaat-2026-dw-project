ab2d63fa06a0e0fe1899a9a0f937c31bd0464001
Gabriel <gabrielreiscunha6@gmail.com>
2026-05-07 21:21:16 -0300

    aula 10 feita

--- app/Controllers/EnvironmentController.js
+++ app/Controllers/EnvironmentController.js
@@ -3,8 +3,20 @@ import path from 'node:path';
 import CONSTANTS from '../../bootstrap/config.js';
 
 export default async function EnvironmentController(request, response) {
-
-
     /** TF 10 - Codar aqui */
 
+    // SE postgres_host for "localhost", está rodando local
+    // SE for qualquer outro valor, está no Docker
+    const ambiente = process.env.POSTGRES_HOST === 'localhost' ? 'local' : 'docker';
+
+    const environment = {
+        NODE_WEB_PORT: process.env.NODE_WEB_PORT,
+        POSTGRES_HOST: process.env.POSTGRES_HOST,
+        POSTGRES_PORT: process.env.POSTGRES_PORT,
+        POSTGRES_USER: process.env.POSTGRES_USER,
+        POSTGRES_DB: process.env.POSTGRES_DB,
+        AMBIENTE: ambiente
+    };
+
+    response.json(environment);
 }
