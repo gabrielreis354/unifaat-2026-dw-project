adc5228dc8b7da1a2b9e0670e6c44e4039e75886
Gabriel <gabrielreiscunha6@gmail.com>
2026-05-07 21:21:33 -0300

    saida formatada

--- app/Controllers/EnvironmentController.js
+++ app/Controllers/EnvironmentController.js
@@ -1,21 +1,17 @@
-import fs from 'node:fs/promises';
-import path from 'node:path';
-import CONSTANTS from '../../bootstrap/config.js';
-
 export default async function EnvironmentController(request, response) {
-    /** TF 10 - Codar aqui */
 
-    // SE postgres_host for "localhost", está rodando local
-    // SE for qualquer outro valor, está no Docker
-    const ambiente = process.env.POSTGRES_HOST === 'localhost' ? 'local' : 'docker';
+    const isDocker = process.env.IS_DOCKER === 'true';
 
     const environment = {
-        NODE_WEB_PORT: process.env.NODE_WEB_PORT,
-        POSTGRES_HOST: process.env.POSTGRES_HOST,
-        POSTGRES_PORT: process.env.POSTGRES_PORT,
-        POSTGRES_USER: process.env.POSTGRES_USER,
-        POSTGRES_DB: process.env.POSTGRES_DB,
-        AMBIENTE: ambiente
+        environment: isDocker ? 'docker' : 'local',
+        database: {
+            host: process.env.POSTGRES_HOST,
+            port: Number(process.env.POSTGRES_PORT)
+        },
+        web: {
+            host: isDocker ? 'nodeweb_host' : 'localhost',
+            port: isDocker ? 8080 : Number(process.env.NODE_WEB_PORT)
+        }
     };
 
     response.json(environment);
