eeb7543c4dcc22d19db300a650a1db6e53875f58
Luan Lourenço <luan.tavares.lourenco@gmail.com>
2026-05-14 10:55:56 -0300

    Aula 11 - Models, Relacionamentos e Timestamps Init Commit

--- routes/apis/addressRouter.js
+++ routes/apis/addressRouter.js
@@ -1,22 +1,9 @@
 import { Router } from 'express';
-import ListAddressController from '../../app/Controllers/AddressApi/ListAddressController.js';
-import GetAddressController from '../../app/Controllers/AddressApi/GetAddressController.js';
-import CreateAddressController from '../../app/Controllers/AddressApi/CreateAddressController.js';
-import UpdateAddressController from '../../app/Controllers/AddressApi/UpdateAddressController.js';
-import DeleteAddressController from '../../app/Controllers/AddressApi/DeleteAddressController.js';
 
 export default (() => {
     const router = Router();
 
-    router.get('/', ListAddressController);
 
-    router.get('/:id', GetAddressController);
-
-    router.post('/', CreateAddressController);
-
-    router.put('/:id', UpdateAddressController);
-
-    router.delete('/:id', DeleteAddressController);
 
     return router;
 })();
